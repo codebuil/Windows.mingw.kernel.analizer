59464a4fa59021f0ca08a88457143081ea577111
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-02-09 20:46:01 +0000

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -4,5 +4,5 @@
 @del *.o   2> temp
 @.\as -o boot.o boot.s
 @.\gcc -c -nostdlib kernel.c -o kernel.o
-@.\ld --entry start > -nostdlib -e 102000 -qmagic  -T link.ld  boot.o kernel.o -o kernel.exe 
+@.\ld --entry start  -nostdlib -e 102000 -qmagic  -T link.ld  boot.o kernel.o -o kernel.exe 
 @.\objdump -M intel  -d kernel.exe 
